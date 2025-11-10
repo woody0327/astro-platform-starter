22835e5dcc7a9b9f7187cb72415e96b8d816de1c
woody0327 <148347940+woody0327@users.noreply.github.com>
2025-11-09 20:09:40 -0500

    Fix typo in cache header comment

--- src/utils.ts
+++ src/utils.ts
@@ -44,11 +44,11 @@ export function generateBlob(parameters?: any) {
 
 export function cacheHeaders(maxAgeDays = 365, cacheTags?: string[]): Record<string, string> {
     // As far as the browser is concerned, it must revalidate on every request.
-    // However, Netlify CDN is told to keep the content cached for up to maxAgeDays (note: new deployment bust the cache by default).
+    // However, Netlify CDN is told to keep the content cached for up to maxAgeDays (note: new deployments bust the cache by default).
     // We're also setting cache tags to be able to later purge via API (see: https://www.netlify.com/blog/cache-tags-and-purge-api-on-netlify/)
     const headers = {
         'Cache-Control': 'public, max-age=0, must-revalidate', // Tell browsers to always revalidate
-        'Netlify-CDN-Cache-Control': `public, max-age=${maxAgeDays * 86_400}, must-revalidate` // Tells Netlify CDN the max allwed cache duration
+        'Netlify-CDN-Cache-Control': `public, max-age=${maxAgeDays * 86_400}, must-revalidate` // Tells Netlify CDN the maximum allowed cache duration
     };
     if (cacheTags?.length > 0) headers['Cache-Tag'] = cacheTags.join(',');
     return headers;
